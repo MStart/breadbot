75007c8ca9369b97fe7e47c7557dc0912749b564
Mark Young <ideamark@qq.com>
2017-09-10 12:35:07 +0800

    Changes to be committed: 	deleted:    breadAI/__pycache__/__init__.cpython-34.pyc 	deleted:    breadAI/core/__pycache__/__init__.cpython-34.pyc 	deleted:    breadAI/core/__pycache__/bot.cpython-34.pyc 	deleted:    breadAI/core/__pycache__/data.cpython-34.pyc 	deleted:    breadAI/core/__pycache__/misc.cpython-34.pyc 	deleted:    breadAI/server/__pycache__/__init__.cpython-34.pyc 	deleted:    breadAI/server/__pycache__/manage.cpython-34.pyc 	deleted:    breadAI/server/server/__pycache__/__init__.cpython-34.pyc 	deleted:    breadAI/server/server/__pycache__/settings.cpython-34.pyc 	deleted:    breadAI/server/server/__pycache__/urls.cpython-34.pyc 	deleted:    breadAI/server/server/__pycache__/wsgi.cpython-34.pyc 	deleted:    breadAI/server/wechat/__pycache__/__init__.cpython-34.pyc 	deleted:    breadAI/server/wechat/__pycache__/urls.cpython-34.pyc 	deleted:    breadAI/server/wechat/__pycache__/views.cpython-34.pyc 	deleted:    breadAI/server/wechat/migrations/__pycache__/__init__.cpython-34.pyc 	modified:   setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,6 @@
 from setuptools import setup
 import sys
 import os
-from breadAI import core
 
 if len(sys.argv) <= 1:
     print('Please enter install or uninstall')
@@ -13,6 +12,7 @@ elif sys.argv[1] == 'install':
     setup(
         setup_requires=['pbr>=0.1'],
         pbr=True,)
+    from breadAI import core
     data_dir = core.misc.get_cfg()['normal']['data_dir']
     db_dir = os.path.join(data_dir, 'data.db')
     if not os.path.exists(db_dir):
